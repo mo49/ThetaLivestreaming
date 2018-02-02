7656ec3cb53362919056008d7a0b0daf4632700d
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2018-02-02 16:37:35 +0900

    auto destroy

---
+++ Assets/Scripts/AutoDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour {
+
+	[SerializeField] float m_lifeSpan;
+
+	void Start() {
+		Invoke("Destroy", m_lifeSpan);
+	}
+
+	void Destroy() {
+		Destroy(gameObject);
+	}
+}
