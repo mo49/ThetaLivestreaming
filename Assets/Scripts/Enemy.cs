761a1640b18d1e2b9a61d4fafdaaa3921ad2ea0f
mo49 <k.mochizuki.0409@gmail.com>
2018-02-04 22:17:53 +0900

    devil move to player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "RealPlayer") {
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+
+			// attack
+		}
+	}
+}
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,10 +7,15 @@ public class EnemyController : MonoBehaviour {
 	[SerializeField] GameObject devilPrefab;
 	[SerializeField] GameObject bossPrefab;
 	[SerializeField] Transform goalTrans;
+	[SerializeField] GameObject enemySpawnArea;
 
 	[SerializeField] int maxEnemyNum;
 
 	private int currentEnemyNum = 0;
+
+	void Start() {
+		StartCreatingEnemy ();
+	}
 
 	void Update() {
 
@@ -31,8 +36,21 @@ public class EnemyController : MonoBehaviour {
 
 	IEnumerator CreateEnemy() {
 		while(currentEnemyNum < maxEnemyNum) {
-			var enemyInstance = Instantiate (devilPrefab, transform.position, Quaternion.identity);
+			var enemyInstance = Instantiate (
+				devilPrefab,
+				transform.position,
+				Quaternion.identity
+			);
+			Transform enemyTrans = enemyInstance.transform;
+			enemyTrans.parent = enemySpawnArea.transform;
+			enemyTrans.localPosition = new Vector3 (
+				Mathf.Sin(Random.Range(-1f,1f)) * 5f,
+				0f,
+				Mathf.Sin(Random.Range(-1f,1f)) * 10f
+			);
+			//enemyInstance.transform.position.y = 0f;
 			enemyInstance.transform.LookAt (goalTrans);
+			enemyInstance.GetComponent<Rigidbody> ().AddForce (enemyInstance.transform.forward * 1, ForceMode.VelocityChange);
 			currentEnemyNum++;
 			yield return new WaitForSeconds (1f);
 		}
