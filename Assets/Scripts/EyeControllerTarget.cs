156b569de76a45ca01bea054320484ec167bf370
mo49 <k.mochizuki.0409@gmail.com>
2018-02-06 23:28:17 +0900

    スイッチに中止した時だけインジケータをまわす

---
+++ Assets/Scripts/EyeControllerTarget.cs
@@ -0,0 +1,41 @@
+﻿//
+//  VR-Studies
+//  Created by miuccie miurror on 11/04/2016.
+//  Copyright 2016 Yumemi.Inc / miuccie miurror
+//
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EyeControllerTarget : MonoBehaviour, EyeController.IEyeControllerTarget {
+
+	[SerializeField] AudioSource m_audio;
+	Color _color;
+
+	void Awake() {
+		Hover(false);
+	}
+
+	public void OnEyeContollerHit( bool isOn ) {
+		// 視線マーカーがヒットしたら
+		Hover(isOn);
+	}
+
+	public void OnEyeContollerClick() {
+		// 視線マーカーでクリック
+		//m_audio.PlayOneShot(m_audio.clip);
+	}
+
+	public void OnTriggerClick(bool isClick) {
+		
+	}
+
+	void Hover(bool isOn, float color=0.7f) {
+		gameObject.GetComponent<Renderer> ().material.color
+				= isOn 
+				? new Color (1f, 1f, 1f)
+				: new Color (color, color, color);
+	}
+
+}
