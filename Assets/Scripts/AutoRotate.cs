4a90a34b8ef075158b4d13b275689c25f7743785
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2018-02-06 15:47:15 +0900

    rotator

---
+++ Assets/Scripts/AutoRotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour {
+
+	float _tumble = 0.3f;
+
+	void Start() {
+		GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * _tumble;
+	}
+}
