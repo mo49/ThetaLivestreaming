f36147a1bee996558028f0cebcf29dc4cb39fab0
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2018-02-06 11:51:03 +0900

    add RealPlayerController

---
+++ Assets/Scripts/RealPlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RealPlayerController : MonoBehaviour {
+
+	[SerializeField] GameObject m_realPlayer;
+
+	bool isActive = true;
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Alpha1)) {
+			Toggle();
+		}
+	}
+
+	void Toggle() {
+		if(isActive) {
+			m_realPlayer.SetActive(false);
+		} else {
+			m_realPlayer.SetActive(true);
+		}
+		isActive = !isActive;
+	}
+}
