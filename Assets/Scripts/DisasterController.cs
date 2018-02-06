2075112e50773714e7e460885f60f8ef2ad6e1d0
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2018-02-06 18:22:49 +0900

    add disaster icons

---
+++ Assets/Scripts/DisasterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisasterController : MonoBehaviour {
+
+	[SerializeField] GameObject[] switchPrefabs;
+	[SerializeField] Transform switchesTrans;
+
+	float _radius = 10f;
+	
+	void Start () {
+		foreach (var switchPrefab in switchPrefabs) {
+			for (int i = 0; i < 2; i++) {
+				var switchInstance = Instantiate(switchPrefab, Random.insideUnitSphere * _radius, Quaternion.identity);
+				switchInstance.transform.parent = switchesTrans;
+			}
+		}
+	}
+
+}
