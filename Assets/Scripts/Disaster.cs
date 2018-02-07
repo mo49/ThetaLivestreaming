0531b0f7add87837d0f57165ef26de32513e4f7e
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2018-02-07 14:33:17 +0900

    攻撃音鳴らす

---
+++ Assets/Scripts/Disaster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disaster : MonoBehaviour {
+
+	[SerializeField] AudioClip[] audioClips;
+
+	AudioSource _audio;
+
+	void Start () {
+		_audio = GetComponent<AudioSource>();
+
+		foreach (var clip in audioClips) {
+			_audio.PlayOneShot(clip);
+		}
+	}
+	
+}
