794e5923c4259b05d2776caca666fe9e25b07e55
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2018-02-09 09:23:22 +0900

    詠唱中エフェクト

--- Assets/Scripts/MeguminController.cs
+++ Assets/Scripts/MeguminController.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class MeguminController : MonoBehaviour {
 
+	[SerializeField] GameObject _castingEffect;
+	[SerializeField] GameObject _shieldEffect;
 	Animator _animator;
+
+	GameObject _castingEffectInstance;
+	GameObject _shieldEffectInstance;
 	void Start () {
 		_animator = GetComponent<Animator>();
 	}
 	
 	public void Idle() {
+		Destroy(_shieldEffectInstance);
+		
 		_animator.SetTrigger("Idle");
 	}
 	public void Explosion() {
+		Destroy(_castingEffectInstance);
 		_animator.SetTrigger("Explosion");
+
+		_shieldEffectInstance = Instantiate(_shieldEffect, transform.position, Quaternion.identity);
 	}
 	public void Casting() {
 		_animator.SetTrigger("Casting");
+		_castingEffectInstance = Instantiate(_castingEffect, transform.position, Quaternion.identity);
 	}
 }
