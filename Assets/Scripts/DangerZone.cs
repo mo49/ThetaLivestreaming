e9e58ab19c21a51113e718703b8e546486922ccd
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2018-02-09 16:24:33 +0900

    flash danger

--- Assets/Scripts/DangerZone.cs
+++ Assets/Scripts/DangerZone.cs
@@ -1,14 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
 
 public class DangerZone : MonoBehaviour {
 	[SerializeField] GameObject _dangerUI;
-
+	[SerializeField] float _angularFrequency = 5f;
+	static readonly float deltaTime = 0.0333f;
+	Text _dangerText;
 	bool isActive = false;
 
 	void Start() {
-
+		_dangerText = _dangerUI.GetComponent<Text>();
+		Frash();
 	}
 
 	void OnTriggerEnter(Collider other) {
@@ -19,9 +25,24 @@ public class DangerZone : MonoBehaviour {
 	}
 
 	public void Hide() {
-		isActive = false;
 		_dangerUI.SetActive(false);
+
+		Invoke("End", 5f);
 	}
 
+	void Frash() {
+		float time = 0f;
+
+		Observable.Interval(TimeSpan.FromSeconds(deltaTime)).Subscribe(_ => {
+			time += _angularFrequency * deltaTime;
+			var color = _dangerText.color;
+			color.a = Mathf.Sin(time) * 0.5f + 0.5f;
+			_dangerText.color = color;
+		}).AddTo(this);
+	}
+
+	void End() {
+		isActive = false;
+	}
 
 }
