1b3c53d64bdb831fbafcc1f1a0f92698b29b472f
mo49 <k.mochizuki.0409@gmail.com>
2018-02-18 02:44:41 +0900

    debug rot

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	float ry;
+	Quaternion _rot;
+
+	void Awake() {
+		_rot = transform.rotation;
+		ry = _rot.y;
+	}
+
+	void Update () {
+		if(Input.GetKey(KeyCode.RightArrow)){
+			ry++;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			ry--;
+		}
+		transform.rotation = Quaternion.Euler (_rot.x, ry, _rot.z);
+	}
+}
